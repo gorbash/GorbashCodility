1dc5585a11bff96b835c573f367907287fe01a7e
Gorbash <radek.stando@gmail.com>
2014-11-16 11:16:18 +0100

    FrogJump finished

---
+++ src/com/gorbash/FrogJump.java
@@ -0,0 +1,16 @@
+package com.gorbash;
+
+/**
+ * Created by Gorbash on 2014-11-16.
+ */
+public class FrogJump {
+
+
+    public int solution(int X, int Y, int D) {
+        int distance = Y-X;
+        int countOfJumps = distance/D;
+        if (D*countOfJumps < distance)
+            countOfJumps++;
+        return countOfJumps;
+    }
+}
---
+++ test/FrogJumpTest.java
@@ -0,0 +1,33 @@
+import com.gorbash.FrogJump;
+import org.junit.Test;
+
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.core.Is.is;
+
+/**
+ * Created by Gorbash on 2014-11-16.
+ */
+public class FrogJumpTest {
+
+    private FrogJump obj = new FrogJump();
+
+    @Test
+    public void testExample() {
+        assertThat(obj.solution(10, 85, 30),is(3));
+    }
+
+    @Test
+    public void testZero() {
+        assertThat(obj.solution(10, 10, 30),is(0));
+    }
+
+    @Test
+    public void testNormal() {
+        assertThat(obj.solution(10, 20, 30),is(1));
+    }
+
+    @Test
+    public void testStepOne() {
+        assertThat(obj.solution(10, 20, 1),is(10));
+    }
+}
