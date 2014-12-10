cdfd389eaa01f2685321933af16c355f3be67b04
Gorbash <radek.stando@gmail.com>
2014-12-10 19:20:07 +0100

    MaxSliceSum

--- src/com/gorbash/maxsliceproblem/MaxSliceSum.java
+++ src/com/gorbash/maxsliceproblem/MaxSliceSum.java
@@ -1,10 +1,34 @@
 package com.gorbash.maxsliceproblem;
 
+import static java.lang.Math.max;
 /**
  * Created by Gorbash on 2014-12-10.
  */
 public class MaxSliceSum {
     public int solution(int[] A) {
-        return 0;
+
+        if (A.length == 0)
+            return 0;
+        if (A.length == 1)
+            return A[0];
+
+        int maxEnding = 0;
+        int maxSlice = 0;
+
+        for (int i = 0; i < A.length; i++) {
+            maxEnding = max(0, maxEnding + A[i]);
+            maxSlice = max(maxSlice, maxEnding);
+        }
+
+        int ret = maxSlice;
+        if (maxSlice == 0) {
+            int maxNegative = Integer.MIN_VALUE;
+            for (int i = 0; i < A.length; i++) {
+                maxNegative = max(maxNegative, A[i]);
+            }
+            ret = maxNegative;
+        }
+
+        return ret;
     }
 }
--- test/com/gorbash/maxsliceproblem/MaxSliceSumTest.java
+++ test/com/gorbash/maxsliceproblem/MaxSliceSumTest.java
@@ -1,7 +1,25 @@
 package com.gorbash.maxsliceproblem;
 
+import org.junit.Test;
+
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.core.Is.is;
+
 /**
  * Created by Gorbash on 2014-12-10.
  */
 public class MaxSliceSumTest {
+
+    private MaxSliceSum obj = new MaxSliceSum();
+
+    @Test
+    public void exampleTest() {
+        assertThat(obj.solution(new int[]{-6, -2}), is(-2));
+        assertThat(obj.solution(new int[]{3, 2, -6, 4, 0}), is(5));
+        assertThat(obj.solution(new int[]{-6, 4, 0, 3}), is(7));
+
+        assertThat(obj.solution(new int[]{-6}), is(-6));
+
+        assertThat(obj.solution(new int[]{0, 0}), is(0));
+    }
 }
